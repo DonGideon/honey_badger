9ebc8f3b9245681a076ddccd18355fc18fcf95f3
Gideon Gur <gideon.gur@seatid.com>
2016-01-02 19:30:10 +0200

    beautify

--- app/controllers/concerns/twitter_api_calls.rb
+++ app/controllers/concerns/twitter_api_calls.rb
@@ -12,23 +12,21 @@ module TwitterApiCalls
     options['Authorization'] = "Basic #{encoded}"
     options['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
 
-    a = res.post('grant_type=client_credentials', options)
-
-    @token = JSON.parse(a)["access_token"]
+    respond = res.post('grant_type=client_credentials', options)
+    token = JSON.parse(respond)["access_token"]
 	end
 
 	def search_call(search_me)
-		auth_call unless @token
+		token = auth_call
 		res = RestClient::Resource.new URI.escape("https://api.twitter.com/1.1/search/tweets.json?q=#{search_me}&lang=en")
 
-    options = {}
-    options['Authorization'] = "Bearer #{@token}"
+    options = {'Authorization' => "Bearer #{token}"}
 
-    a = res.get(options)
+    respond = res.get(options)
 
     keys = %w{created_at text}
     user_keys = %w{profile_image_url_https name screen_name}
-    JSON.parse(a)["statuses"].inject([]){|arr, h| arr << (h.select{|k,v| keys.include? k}).merge(h["user"].select{|kk,vv| user_keys.include? kk}); arr}
+    JSON.parse(respond)["statuses"].inject([]){|arr, h| arr << (h.select{|k,v| keys.include? k}).merge(h["user"].select{|kk,vv| user_keys.include? kk}); arr}
 	end
 
 end
