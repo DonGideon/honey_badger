b03ef3e085f88ec8eabdb6522b1859729efb493f
Gideon Gur <gideon.gur@seatid.com>
2016-01-03 01:18:04 +0200

    test wip

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not create user with the same name" do
+  	users(:user_one)
+    user = User.new(name: "user_one")
+    assert user.invalid?
+    assert_equal ["has already been taken"] ,user.errors[:name]
+  end
+
+  test "should not create user with empty name" do
+    user = User.new(name: nil)
+    assert user.invalid?
+    assert_equal ["can't be blank"] ,user.errors[:name]
+  end
 end
